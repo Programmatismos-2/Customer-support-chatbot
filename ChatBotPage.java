890a7d604a07be648925410fb1571a4fdb9fb9c6
Marissa Filippou <t8220160@aueb.gr>
2023-11-18 14:46:08 +0200

    Added request for user verification in ChatBotPage

--- ChatBotPage.java
+++ ChatBotPage.java
@@ -1,3 +1,9 @@
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class ChatBotPage {
     public void openChatbotWindow() {
         JFrame chatbotFrame = new JFrame("Εξυπηρέτηση Πελατών - Chatbot");
@@ -5,5 +11,36 @@ public class ChatBotPage {
         chatbotFrame.setSize(600, 400);
         chatbotFrame.setVisible(true);  //Κάνει ορατό το νέο παράθυρο
         chatbotFrame.setIconImage(logo.getImage());
+        chatbotFrame.setLayout(null);
+        
+        JTextField surnameF2 = new JTextField();
+        JTextField flightF2 = new JTextField();
+        JLabel surnameL2 = new JLabel("Surname");
+        JLabel flightL2 = new JLabel("Flight Number");
+
+        surnameF2.setBounds(200, 140, 200, 30);
+        flightF2.setBounds(200, 200, 200, 30);
+        surnameL2.setBounds(50, 140, 200, 30);
+        flightL2.setBounds(50, 200, 200, 30);
+
+        chatbotFrame.add(surnameF2);
+        chatbotFrame.add(flightF2);
+        chatbotFrame.add(surnameL2);
+        chatbotFrame.add(flightL2);
+
+        JButton submi1tButton = new JButton("Submit");
+        submit1Button.setBounds(450,300, 100, 30);
+        submit1Button.setBorder(BorderFactory.createEtchedBorder());
+        chatbotFrame.add(submit1Button);
+
+        submit1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*Mediator med1 = new Mediator();    //Φτιάχνει αντικείμενο από την Mediator Class
+                med1.dataInput(surnameF2.getText(), flightF2.getText());    //Καλεί την μέθοδο του Mediator για εισαγωγή δεδομένων
+                */
+                chatbotFrame.setVisible(false);
+            }
+        });
     }
 }
--- CheckInPage.java
+++ CheckInPage.java
@@ -7,38 +7,35 @@ public class CheckInPage {
         checkFrame.setLayout(null);
         checkFrame.setIconImage(logo.getImage());
 
-        JTextField nameF = new JTextField();
-        JTextField surnameF = new JTextField();
-        JTextField flightF = new JTextField();
-        JTextField emailF = new JTextField();
+        JTextField nameF1 = new JTextField();
+        JTextField surnameF1 = new JTextField();
+        JTextField flightF1 = new JTextField();
+        JTextField emailF1 = new JTextField();
         
         JLabel nameL1 = new JLabel("Name"); 
         JLabel surnameL1 = new JLabel("Surname");
         JLabel flightL1 = new JLabel("Flight Number");
         JLabel emailL1 = new JLabel("Email");
 
-
-
         nameL1.setBounds(50, 50, 100, 30);
         surnameL1.setBounds(50, 110, 100, 30);
         flightL1.setBounds(50, 170, 100, 30);
         emailL1.setBounds(50, 230, 100, 30);
 
-        nameF.setBounds(200, 50, 200, 30);
-        surnameF.setBounds(200, 110, 200, 30);
-        flightF.setBounds(200, 170, 200, 30);
-        emailF.setBounds(200, 230, 200, 30);
-        
+        nameF1.setBounds(200, 50, 200, 30);
+        surnameF1.setBounds(200, 110, 200, 30);
+        flightF1.setBounds(200, 170, 200, 30);
+        emailF1.setBounds(200, 230, 200, 30);
         
         checkFrame.add(nameL1);
         checkFrame.add(surnameL1);
         checkFrame.add(flightL1);
         checkFrame.add(emailL1);
 
-        checkFrame.add(nameF);
-        checkFrame.add(surnameF);
-        checkFrame.add(flightF);
-        checkFrame.add(emailF);
+        checkFrame.add(nameF1);
+        checkFrame.add(surnameF1);
+        checkFrame.add(flightF1);
+        checkFrame.add(emailF1);
 
         JButton submitButton = new JButton("Submit");
         submitButton.setBounds(450,300, 100, 30);
@@ -49,7 +46,7 @@ public class CheckInPage {
             @Override
             public void actionPerformed(ActionEvent e) {
                 /*Mediator med = new Mediator();    //Φτιάχνει αντικείμενο από την Mediator Class
-                med.dataInput(nameF.getText(), surnameF.getText(), flightF.getText(), emailF.getText());    //Καλεί την μέθοδο του Mediator για εισαγωγή δεδομένων
+                med.dataInput(nameF1.getText(), surnameF1.getText(), flightF1.getText(), emailF1.getText());    //Καλεί την μέθοδο του Mediator για εισαγωγή δεδομένων
                 */
                 checkFrame.setVisible(false);
             }
