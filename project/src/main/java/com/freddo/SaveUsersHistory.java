64ee06ac9e35e205911749d73523f51b6e56de54
aphroditebe <148347246+aphroditebe@users.noreply.github.com>
2023-12-05 18:55:50 +0200

    Add files via upload

---
+++ project/src/main/java/com/freddo/SaveUsersHistory.java
@@ -0,0 +1,47 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import project.src.main.java.com.freddo.Ai;
+
+public class SaveUsersHistory {
+
+    public static String SaveEmailAndQuestion(String email,String Question) {
+
+
+        String csvFilePath = "your_file.csv";
+
+      //  Ai aiobj = new Ai();
+        String inputText = Ai.hey();//θα καλώ την AI
+        String textWithoutCommas = removeCommas(inputText);
+        System.out.println("Original Text: " + inputText);
+        System.out.println("Text without Commas: " + textWithoutCommas);
+
+        saveDataToCSV(email, Question, textWithoutCommas, csvFilePath);
+        return inputText;
+
+    }
+
+    private static void saveDataToCSV(String email, String question, String answer, String csvFilePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath, true))) {
+            // Append a new line with email, question, and answer
+            writer.write(email + "," + question + "," + answer);
+            writer.newLine();
+
+            System.out.println("Data has been written to the CSV file.");
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception according to your needs
+        }
+    }
+
+    public static void main(String[] args){
+        SaveEmailAndQuestion("aphroditebenou@gmail.com","Hi");
+    }
+
+
+    private static String removeCommas(String inputText) {
+        // Use the replace method to remove all commas
+        return inputText.replaceAll(",", "");
+    }
+}
