5fd8e1c04bd3b23f8065a8270b599d547527f827
fanisorfan <147718479+fanisorfan@users.noreply.github.com>
2024-01-06 16:40:10 +0200

    Update RunBashCommand.java

--- project/src/main/java/com/freddo/RunBashCommand.java
+++ project/src/main/java/com/freddo/RunBashCommand.java
@@ -1,18 +1,24 @@
 package com.freddo;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// RunBashCommand class for executing bash commands
 public class RunBashCommand {
+
+    // Method to obtain an OAuth2 access token using the gcloud command
     public static String aoth2() throws InterruptedException {
         try {
             // Specify the gcloud command you want to run
             String command = "gcloud auth print-access-token";
-            // Create a ProcessBuilder
+            
+            // Create a ProcessBuilder for running the command
             ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
            
             // Redirect error stream to output stream
             processBuilder.redirectErrorStream(true);
+            
             // Start the process
             Process process = processBuilder.start();
 
@@ -20,12 +26,19 @@ public class RunBashCommand {
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             String key = "";
+            
+            // Collect the lines of output
             while ((line = reader.readLine()) != null) {
                 key += line;
             }
-            // Wait for the process to complete           
+            
+            // Wait for the process to complete
+            process.waitFor();
+            
+            // Return the obtained OAuth2 access token
             return key;
         } catch (IOException e) {
+            // Handle IOException if any
             return "error";
         }
     }
