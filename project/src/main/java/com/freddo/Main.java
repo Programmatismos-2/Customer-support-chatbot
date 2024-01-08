47b6f962f3ea6460a7d24e7a613a41a88f743707
georgeflour <147036073+georgeflour@users.noreply.github.com>
2024-01-09 00:09:03 +0200

    Update Main.java

--- project/src/main/java/com/freddo/Main.java
+++ project/src/main/java/com/freddo/Main.java
@@ -1,7 +1,9 @@
 package com.freddo;
 
 import java.io.IOException;
-
+/*
+ * The main class that runs the program
+ */
 public class Main {
     public static void main(String args[]) throws IOException, InterruptedException {
         new Home_Page();
