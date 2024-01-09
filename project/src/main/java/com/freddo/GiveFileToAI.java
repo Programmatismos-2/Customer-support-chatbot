b81ccaac76c93d36002f17d22ddf2b6b9884135e
georgeflour <147036073+georgeflour@users.noreply.github.com>
2024-01-09 19:04:12 +0200

    Update GiveFileToAI.java

--- project/src/main/java/com/freddo/GiveFileToAI.java
+++ project/src/main/java/com/freddo/GiveFileToAI.java
@@ -1,24 +1,19 @@
 package com.freddo;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
 /**
  * GiveFileToAI class for reading a file and preparing data for AI.
  */
 public class GiveFileToAI {
-
     public static String fileToVariable(String userquestion) throws IOException {
         // Specify the path to the file
         String filePath = "/path/to/testTermsandConditions.txt";
 
         // Create a BufferedReader to read the file
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
-
         String line = null;
         StringBuilder stringBuilder = new StringBuilder();
-
         try {
             // Read each line from the file and append it to the StringBuilder
             while ((line = reader.readLine()) != null) {
