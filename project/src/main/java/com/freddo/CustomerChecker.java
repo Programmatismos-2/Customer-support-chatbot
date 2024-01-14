ed63755c6f2db306a73dc1d8f3642ef37ad7c7ff
fanisorfan <147718479+fanisorfan@users.noreply.github.com>
2024-01-14 23:02:51 +0200

    Update CustomerChecker.java

--- project/src/main/java/com/freddo/CustomerChecker.java
+++ project/src/main/java/com/freddo/CustomerChecker.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024, FreddoEspresso
+ * All rights reserved.
+ *
+ * This source code is licensed under the Academic Free License v. 3.0.
+ * See LICENSE.txt or http://opensource.org/licenses/AFL-3.0 for more details.
+ */
+
 package com.freddo;
 import java.io.BufferedReader;
 import java.io.FileReader;
