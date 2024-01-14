6647ef7fabbbf8010ac3d30ad9bd490a3e98fca6
georgeflour <147036073+georgeflour@users.noreply.github.com>
2024-01-14 20:22:27 +0200

    Update CustomerChecker.java

--- project/src/main/java/com/freddo/CustomerChecker.java
+++ project/src/main/java/com/freddo/CustomerChecker.java
@@ -8,7 +8,8 @@ import java.io.IOException;
  */
 public class CustomerChecker {
     public static String checkCustomerExistence(String lastName, String flightNumber) {
-        String csvFilePath = "/path/to/output.csv"; //TODO change the path to the apropriate one 
+        //TODO change the path to the apropriate one
+        String csvFilePath = "/path/to/output.csv";
         try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
